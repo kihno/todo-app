23e8292290d5fb3a335876b7e642cf9e72541fbe
kihno <stistyle13@gmail.com>
2022-04-18 16:59:33 -0700

    Initialize webpack bundle

---
+++ src/todos.js
@@ -0,0 +1,29 @@
+const todos = (function() {
+    const todos = [];
+
+    //cache DOM
+    const title = document.getElementById('title');
+    const description = document.getElementById('description');
+    const dueDate = document.getElementById('dueDate');
+    const priority = document.getElementById('priority');
+    const submit = document.getElementById('submit');
+
+    //bind events
+
+    //functions
+    function render() {
+
+    }
+
+    function addTodo() {
+
+    }
+
+    function completeTodo() {
+        
+    }
+
+    function removeTodo() {
+
+    }
+})();
